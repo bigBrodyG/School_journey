cc3d5ea1843ecd309df12f5a0bfaf9921c2fea0c
GiordanoFornari <gfornari.casa@gmail.com>
2025-03-13 11:01:59 +0100

    Impiccato level up

--- 💾informatica/impiccato.py
+++ 💾informatica/impiccato.py
@@ -1,6 +1,19 @@
 import random
 import os
 
+winner_message = """
+      ___________
+     \'._==_==_=_.\'
+     .-\:      /-.
+    | (|:.     |) |
+     \'-|:.     |-\'
+       \::.    /
+        \'::. .\'
+          ) (
+        _.\' \'._
+       `\"\"\"\"\"\"\"`
+"""
+
 greeting = """
 Benvenuto in Impiccato!
 Il gioco consiste nel cercare di indovinare una parola
@@ -8,12 +21,35 @@ indovinando lettere una alla volta.
 Se credi di sapere la parola, puoi anche scriverla interamente.
 Buon divertimento!
 """
-with open("parole.txt", "r") as f:
+print("""
+__________________________________________
+||                                        ||
+||  Seleziona il livello di difficoltà:   ||
+||       1. Facile                        ||
+||       2. Medio                         ||
+||       3. Difficile                     ||
+||________________________________________||
+      """)
+match input("Seleziona il livello : "):
+    case "1":
+        print("Livello Facile")
+        disctionary = "parole.txt"
+    case "2":
+        print("Livello Medio")
+        disctionary = "parole2.txt"
+    case "3":
+        print("Livello Difficile")
+        disctionary = "parole3.txt"
+    case default:
+        print("Livello non valido")
+
+with open(disctionary, "r") as f:
     parole = f.readlines()
     parola = random.choice(parole).strip()
 errori = 0
 user_word = ""
 letters = ""
+
 displayed_word = ""
 def get_hangman_stage(errors):
     hangman_stages = [
@@ -182,9 +218,14 @@ while displayed_word != parola:
             errori += 1
     if errori >= 6:
         print(get_hangman_stage(6))
-        print("Hai perso!")
+        print("\t\t\tHai perso!")
         print(f"La parola era: {parola}\n")
 #        print("Sei un fallito!")
-        break
-    displayed_word.replace((' ', '') for " " in letters)
-
+        exit(0)
+    displayed_word = ''.join([letter if letter in letters else '' for letter in parola])
+try:
+    os.system("clear")
+except:
+    pass
+print(winner_message)
+print("Complimenti!!\n\nHai Vinto!!")
