15d205284db2742ce31cbd5be0b2ddca1889b422
GiordanoFornari <gfornari.casa@gmail.com>
2025-04-09 22:27:06 +0200

    .gitignore is now working

--- 🛠️SISTEMI/.AUTOMAZONE/C LANG/ES_19_12/liiib.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIB_H_INCLUDE
-#define LIB_H_INCLUDE
-float somma(float a, float b);
-float diff(float a, float b);
-float molt(float a, float b);
-float divis(float a, float b);
-#endif
--- 🛠️SISTEMI/.AUTOMAZONE/C LANG/LEZ_02/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int num;
-    printf("Inserisci un numero intero: ");
-    scanf("%d", &num );
-    printf("Hai inserito il valore: %d",num);
-    printf("\n");
-    return 0; 
-}
