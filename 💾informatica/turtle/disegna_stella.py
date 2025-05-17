0ee2339f7cf4f0fd3b27680ead1f71f7a3d45862
Giordano Fornari <gfornari.casa@gmail.com>
2025-05-17 13:31:12 +0200

    Update disegna_stella.py

--- 💾informatica/turtle/disegna_stella.py
+++ 💾informatica/turtle/disegna_stella.py
@@ -1,12 +1,57 @@
 import turtle as t
+import math
 
 t.speed(0)
 t.penup()
 t.pencolor("red")
 t.fillcolor("yellow")
+t.screensize(800, 600)
 
-def disegna_stella(lato):
-    #t.goto(t.xcor()-lato/2, t.ycor()+lato/2)
+def disegna_fiocco6(lato: float) -> None:
+    start_pos = t.position()
+    start_heading = t.heading()
+
+    for x in range(2):
+        # imposta lo spessore della penna
+        t.pensize(5 if x else 0)
+
+        # triangolo superiore
+        t.penup()
+        t.setposition(start_pos)          # riparto sempre dal centro
+        t.setheading(start_heading)       # e con la stessa direzione
+        t.forward(lato/2)
+        t.left(90)
+        t.forward(lato/2)
+        t.right(90)
+        t.pendown()
+        if x == 0: t.begin_fill()
+        for _ in range(3):
+            t.forward(lato)
+            t.right(120)
+        if x == 0: t.end_fill()
+
+        # triangolo inferiore
+        t.penup()
+        t.setposition(start_pos)
+        t.setheading(start_heading)
+        t.forward(lato/2)
+        t.right(90)
+        t.forward(lato/ int(math.sqrt(lato)))
+        t.left(90)
+        t.pendown()
+        if x == 0: t.begin_fill()
+        for _ in range(3):
+            t.forward(lato)
+            t.left(120)
+        if x == 0: t.end_fill()
+        t.penup()
+    t.setposition(start_pos)
+    t.setheading(start_heading)
+
+def disegna_stella(lato: float) -> None:
+    start_pos = t.position()
+    start_heading = t.heading()
+    t.pensize()
     t.pendown()
     t.begin_fill()
     for _ in range(5):
@@ -15,28 +60,14 @@ def disegna_stella(lato):
     t.end_fill()
     t.penup()
 
-def disegna_fiocco6(lato):
-    t.pensize(5)
-    t.goto(-lato/2, lato/2)
-    t.pendown()
-    t.begin_fill()
-    for _ in range(3):
-        t.forward(lato)
-        t.right(360/3)
-    t.end_fill()
-    t.penup()
-    t.goto(-lato/2, -lato/8)
-    t.pendown()
-    t.begin_fill()
-    for _ in range(3):
-        t.forward(lato)
-        t.left(360/3)
-    t.end_fill()
-    t.penup()
+    t.setposition(start_pos)
+    t.setheading(start_heading)
+    t.stamp()
 
-disegna_fiocco6(100)
-t.goto(100, 0)
+
+
 disegna_stella(100)
-
+t.goto(100, -300)
+disegna_fiocco6(100)
 
 t.done()
