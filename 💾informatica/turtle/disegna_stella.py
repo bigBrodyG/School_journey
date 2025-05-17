01f6fb143394a22ecfe3b15969a6e142df99da32
Giordano Fornari <gfornari.casa@gmail.com>
2025-05-17 13:28:30 +0200

    Update disegna_stella.py

--- 💾informatica/turtle/disegna_stella.py
+++ 💾informatica/turtle/disegna_stella.py
@@ -1,22 +1,22 @@
 import turtle as t
 
+t.speed(0)
+t.penup()
+t.pencolor("red")
+t.fillcolor("yellow")
+
 def disegna_stella(lato):
-    t.speed(0)
-    t.penup()
-    t.goto(0, 0)
+    #t.goto(t.xcor()-lato/2, t.ycor()+lato/2)
     t.pendown()
     t.begin_fill()
     for _ in range(5):
         t.forward(lato)
         t.right(144)
     t.end_fill()
+    t.penup()
 
 def disegna_fiocco6(lato):
-    t.speed(0)
-    t.penup()
     t.pensize(5)
-    t.pencolor("red")
-    t.fillcolor("yellow")
     t.goto(-lato/2, lato/2)
     t.pendown()
     t.begin_fill()
@@ -32,8 +32,11 @@ def disegna_fiocco6(lato):
         t.forward(lato)
         t.left(360/3)
     t.end_fill()
+    t.penup()
 
-disegna_fiocco6(200)
+disegna_fiocco6(100)
+t.goto(100, 0)
+disegna_stella(100)
 
 
 t.done()
